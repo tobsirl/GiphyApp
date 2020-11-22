1372b35e9286aba6cbab52a8c9301780881cb432
Paul Tobin <tobster111@gmail.com>
2020-11-22 15:29:41 +0000

    Added routing + .env file for the api key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Random from './Pages/Random';
 
 function App() {
   return (
     <div className="App">
-      <h1>React</h1>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/random">
+          <Random />
+        </Route>
+      </Switch>
     </div>
   );
 }
