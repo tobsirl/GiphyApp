bf3c384f3062511fab8b7a6b8e9dc8dba95e6d2c
Paul Tobin <tobster111@gmail.com>
2020-11-24 10:54:13 +0000

    Styling on the random gif

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 React application using [Giphy Api](https://developers.giphy.com/)
 
 ## Features
+* Fetches a list of trending gifs from the Giphy API
+* Allows the user to search for gifs
+* Responsive design using Flexbox
 ## Hosted
 Hosted on AWS using Amplify [Link](https://master.d1fpyj0wm36a47.amplifyapp.com/)
+## Routing
+| Route   | Component | Description                                                    |
+|---------|-----------|----------------------------------------------------------------|
+| /       | Home      | Displays a list of “trending” gifs, ability to search for gifs |
+| /random | Random    | Displays a random gif                                          |
 
 
-## Endpoints used
+## API Endpoints used
 | Endpoint | Request URL                                                                                                         |
 |----------|---------------------------------------------------------------------------------------------------------------------|
 | Trending | https://api.giphy.com/v1/gifs/trending?api_key=yaV41KWzdrpCOtWDwqW6GEcC9nf2LpqX&limit=25&rating=g                   |
--- src/Components/RandomGiphy.js
+++ src/Components/RandomGiphy.js
@@ -1,23 +1,77 @@
-// import { faDiceD6 } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Proptypes from 'prop-types';
+import setColorRating from '../utils/setColorRating';
+import truncateString from '../utils/truncateString';
+
 import { GiRollingDices } from 'react-icons/gi';
 export default function RandomGiphy(props) {
-  const { title, id, images } = props.data;
-  console.log(props.data);
+  const {
+    title = 'No Title',
+    id,
+    images,
+    rating = 'g',
+    type,
+    username,
+    source_tld,
+    import_datetime,
+    url,
+  } = props.data;
+
   return (
-    <div className="random">
-      <div className="random__btn">
-        <button
-          onClick={() => props.rollDice(props.roll === true ? false : true)}
-          className="btn-dice"
-        >
-          {/* <FontAwesomeIcon icon={faDiceD6} size="6x" /> */}
-          <GiRollingDices />
-        </button>
+    <>
+      <div className="random">
+        <div className="random__btn">
+          <button
+            onClick={() => props.rollDice(props.roll === true ? false : true)}
+            className="btn-dice"
+          >
+            <GiRollingDices />
+          </button>
+        </div>
+
+        <div className="giphy">
+          <img src={images?.original.url} alt="title" />
+
+          <div className="giphy__info">
+            <p>{truncateString(title, 40)}</p>
+            <span className={`tag ${setColorRating(rating)}`}>
+              {rating.toUpperCase()}
+            </span>
+          </div>
+
+          <div className="giphy__details">
+            <h3>Details</h3>
+            <p>
+              <strong>ID:</strong> {id}
+            </p>
+            <p>
+              <strong>Type:</strong> {type}
+            </p>
+            <p>
+              <strong>Username:</strong> {username}
+            </p>
+            <p>
+              <strong>Source:</strong> {source_tld}
+            </p>
+            <p>
+              <strong>Import Date & time:</strong> {import_datetime}
+            </p>
+            <a className="btn" href={url} target="_blank" rel="noreferrer">
+              View on Giphy
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="random__image">
-        <img src={images?.original.url} alt="title" />
-      </div>
-    </div>
+    </>
   );
 }
+
+RandomGiphy.propTypes = {
+  id: Proptypes.string.isRequired,
+  title: Proptypes.string.isRequired,
+  rating: Proptypes.string.isRequired,
+  images: Proptypes.object.isRequired,
+  import_datetime: Proptypes.string.isRequired,
+  type: Proptypes.string.isRequired,
+  username: Proptypes.string.isRequired,
+  source_tld: Proptypes.string.isRequired,
+};
